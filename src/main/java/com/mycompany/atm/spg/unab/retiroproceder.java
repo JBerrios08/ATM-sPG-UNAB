6c25ff3786209a1fcb2b779bc03533a80dc2b5cf
Jaime Fernando Berrios Ortiz <162358684+JBerrios08@users.noreply.github.com>
2026-04-17 11:00:54 -0600

    Mostrar monto dinámico en confirmación de retiro

--- src/main/java/com/mycompany/atm/spg/unab/retiroproceder.java
+++ src/main/java/com/mycompany/atm/spg/unab/retiroproceder.java
@@ -1,13 +1,17 @@
 package com.mycompany.atm.spg.unab;
 
 import java.awt.EventQueue;
+import java.text.NumberFormat;
+import java.util.Locale;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
 public class retiroproceder extends javax.swing.JFrame {
 
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(retiroproceder.class.getName());
+    private final NumberFormat formatoMoneda = NumberFormat.getIntegerInstance(new Locale("es", "SV"));
 
     public retiroproceder() {
         initComponents();
@@ -18,9 +22,7 @@ public class retiroproceder extends javax.swing.JFrame {
 
         PanelFondoResponsivo panel = new PanelFondoResponsivo("/imagenes/PROCEDERRETIRO.png", 1920, 1080);
 
-        javax.swing.JLabel titulo = UIHelper.createOverlayLabel(
-                UIHelper.textoEnDosLineas("¿ES CORRECTA LA CANTIDAD", "A RETIRAR?"),
-                48);
+        JLabel titulo = UIHelper.createOverlayLabel(crearTextoConfirmacion(FlujoATM.getInstance().getMontoSeleccionado()), 42);
 
         JButton btnNo = UIHelper.createTransparentButton("NO", 48);
         btnNo.addActionListener(this::btnCantidadNoCorrectaActionPerformed);
@@ -28,13 +30,22 @@ public class retiroproceder extends javax.swing.JFrame {
         JButton btnSi = UIHelper.createTransparentButton("SÍ", 48);
         btnSi.addActionListener(this::btnCantidadSiCorrectaActionPerformed);
 
-        panel.add(titulo, new RestriccionesRelativas(0.224, 0.270, 0.552, 0.105));
+        panel.add(titulo, new RestriccionesRelativas(0.224, 0.245, 0.552, 0.180));
         panel.add(btnNo, new RestriccionesRelativas(0.083, 0.741, 0.090, 0.102));
         panel.add(btnSi, new RestriccionesRelativas(0.839, 0.769, 0.090, 0.102));
 
         setContentPane(panel);
         pack();
         setLocationRelativeTo(null);
+    }
+
+    private String crearTextoConfirmacion(Integer montoSeleccionado) {
+        if (montoSeleccionado == null) {
+            return UIHelper.textoEnDosLineas("¿ES CORRECTA LA CANTIDAD", "A RETIRAR?");
+        }
+
+        String montoFormateado = "$" + formatoMoneda.format(montoSeleccionado);
+        return "<html><div style='text-align:center;'>¿ES CORRECTA LA CANTIDAD<br>A RETIRAR?<br>" + montoFormateado + "</div></html>";
     }
 
     private void btnCantidadNoCorrectaActionPerformed(java.awt.event.ActionEvent evt) {
